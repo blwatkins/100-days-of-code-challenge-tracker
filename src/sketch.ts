cad566780f9d1b84cd5254aec65c4fbf02f339aa
blwatkins <blwatkins@users.noreply.github.com>
2024-08-08 00:41:18 -0500

    Update sketch.
    * Add circle styling.
    * Add canvas context.
    * Add random background color
    * Add TODOs for genart library

--- src/sketch.ts
+++ src/sketch.ts
@@ -29,12 +29,19 @@ import {
 
 import '../assets/styles/sketch.css';
 
+import { CanvasContext } from './canvas-context';
+
 function sketch(p5: P5Lib): void {
     let selector: ColorSelector;
+    const count: number = 1;
     const xs: number[] = [];
     const ys: number[] = [];
     const ds: number[] = [];
     const colors: Color[] = [];
+    const outlines: Color[] = [];
+    let isShowing: boolean = false;
+    let max: number;
+    let background: Color;
 
     const buildColorSelectorManager = (): ColorSelectorManager => {
         const manager: ColorSelectorManager = new ColorSelectorManager();
@@ -47,24 +54,47 @@ function sketch(p5: P5Lib): void {
     };
 
     p5.setup = (): void => {
-        const canvas: P5Lib.Renderer = p5.createCanvas(1080, 1080, p5.WEBGL);
-        canvas.attribute('style', 'height: 100vh;');
         SketchContext.initialize(p5);
+        CanvasContext.buildCanvas(1152, 1536);
+        // 2K - 3:4 Ratio - 1152, 1536
+        // 2K - 1:1 Ratio - 2048, 2048
+
         const manager: ColorSelectorManager = buildColorSelectorManager();
         selector = manager.getRandomColorSelector();
+        max = Math.max(p5.width, p5.height);
+        // TODO - color selectors should return a COPY of the color. Not the color object.
+        // TODO - Color copy method
+        // TODO - Color constructor that accepts a Color object
+        // TODO - Fix p5 context retrieval in all classes that use it. You have to get it inside each method so it stays up to date
+        background = selector.getBackgroundColor(0.3, 0.5, 0.2);
 
-        for (let i: number = 0; i < 10; i++) {
-            xs.push(Random.randomFloat((p5.width / 2.0) * -1, (p5.width / 2.0)));
-            ys.push(Random.randomFloat((p5.height / 2.0) * -1, (p5.height / 2.0)));
-            ds.push(Random.randomFloat(10, 100));
-            colors.push(selector.getColor());
+        for (let i: number = 0; i < count; i++) {
+            const d: number = Random.randomFloat(10, max * 0.2);
+            const r: number = d / 2.0;
+            const color: Color = new Color(selector.getColor().color);
+            color.alpha = Math.floor(185);
+            const outline: Color = new Color(selector.getColor().color);
+            outline.alpha = 200;
+            xs.push(Random.randomFloat(CanvasContext.minWidth + r, CanvasContext.maxWidth - r));
+            ys.push(Random.randomFloat(CanvasContext.minHeight + r, CanvasContext.maxHeight - r));
+            ds.push(d);
+            colors.push(color);
+            outlines.push(outline);
         }
     };
 
     p5.draw = (): void => {
-        for (let i: number = 0; i < 10; i++) {
-            p5.fill(colors[i].color);
-            p5.ellipse(xs[i], ys[i], ds[i], ds[i]);
+        if (!isShowing) {
+            p5.background(background.color);
+            p5.strokeWeight((max * 0.001) * 5);
+
+            for (let i: number = 0; i < count; i++) {
+                p5.stroke(outlines[i].color);
+                p5.fill(colors[i].color);
+                p5.ellipse(xs[i], ys[i], ds[i], ds[i]);
+            }
+
+            isShowing = true;
         }
     };
 }
