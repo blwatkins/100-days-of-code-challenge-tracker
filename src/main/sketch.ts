1f31ae6ec0d345c385ace7659b6dfd6e55b085c7
blwatkins <blwatkins@users.noreply.github.com>
2024-12-18 19:38:33 -0600

    Update copyright comments

--- src/main/sketch.ts
+++ src/main/sketch.ts
@@ -1,1 +1,24 @@
+/*
+ * Copyright (C) 2024 brittni and the polar bear LLC.
+ *
+ * This file is a part of brittni's #100DaysOfCode tracking project,
+ * which is released under the GNU Affero General Public License, Version 3.0.
+ * You may not use this file except in compliance with the license.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. See LICENSE or go to
+ * https://www.gnu.org/licenses/agpl-3.0.en.html for full license details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * The visual outputs of this source code are licensed under the
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0) License.
+ * You should have received a copy of the CC BY-NC-ND 4.0 License with this program.
+ * See OUTPUT-LICENSE or go to https://creativecommons.org/licenses/by-nc-nd/4.0/
+ * for full license details.
+ */
+
 console.log('Hello, world!');
